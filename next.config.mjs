52b85f1c434e31b5ec45c201d0e0008e4fd63ec0
Zah-id <zahidpulo53@gmail.com>
2026-09-05 19:39:59 +0700

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: isProd ? '/PortoPendidikan-UI-UX' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
